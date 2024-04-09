5dfd446fb66d7a788bb5020c9eefcefaae51b611
jahnirgens <61604346+jahnirgens@users.noreply.github.com>
2024-04-09 11:00:16 +0200

    Start

---
+++ topmenu.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="ommeg/index.php">Om meg</a></li>
+    <li><a href="#news">Utvikling</a></li>
+    <li><a href="#contact">Drift</a></li>
+    <li><a href="#about">Brukerstøtte</a></li>
+    <li style="float:right"><a class="active" href="#about">Om siden</a></li>
+</ul>  
